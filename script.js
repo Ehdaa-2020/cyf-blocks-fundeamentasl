41962d76abeabcba20f1547b8da45b70cea3ca59
Ehdaa-2020 <109866621+Ehdaa-2020@users.noreply.github.com>
2022-08-06 15:11:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-/* paste the JavaScript code from the 'Generated Code' section of the CYF Blocks editor here */
+var links, item;
+
+
+links = ['https://www.ebay.co.uk/', 'https://www.childsavers.org/', 'https://www.gla.ac.uk/'];
+item = ['ebay', 'childsavers', 'university of Glasgow'];
+let element_list = document.getElementById('list');
+while (!!links.length) {
+  if(--window.LoopTrap <= 0) throw "Infinite loop.";
+  let new_li = document.createElement('li');
+  let new_a = document.createElement('a');
+  new_a.setAttribute("href", links.shift());
+  new_a.innerText = item.shift();
+
+  new_li.appendChild(new_a);
+
+  element_list.appendChild(new_li);
+}
